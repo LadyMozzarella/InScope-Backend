56d1ddfdba95cbad6c092049b70c3f33710ee908
Brittany Mazza <bmazza555@gmail.com>
2017-05-06 13:04:01 -0700

    Return 204 no content for favicon

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// We don't have a favicon
+router.get('/favicon.ico', function(req, res) {
+  res.sendStatus(204);
+});
+
 module.exports = router;
